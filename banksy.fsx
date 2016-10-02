a9d93f559304210406afdc237d1ee7c1d4312480
Ben Lynch <blynch23@ntlworld.com>
2016-10-02 17:46:34 +0100

    Initial version with web bits working.

--- banksy.fsx
+++ banksy.fsx
@@ -1,5 +1,6 @@
 ﻿#r"packages/FSharp.Data/lib/net40/FSharp.Data.dll"
 #r"packages/Suave/lib/net40/Suave.dll"
+#load"async.fs"
 open System
 open System.Collections.Generic
 open System.Text.RegularExpressions
@@ -13,6 +14,8 @@ open Suave.Sockets.Control
 open Suave.Sockets.AsyncSocket
 open Suave.WebSocket
 open Suave.Utils
+open AsyncHelpers
+
 let firstOrNone seq = Seq.tryFind (fun _ -> true) seq
 
 type Event =
@@ -23,11 +26,7 @@ type Event =
       Long : float 
       ImgSrc : string
     }
-with static member (-) (ev1, ev2) = 
-        let varDays = (((ev1.Occurred - ev2.Occurred).Days) |> float) ** 2.
-        let varLat  = (ev1.Lat - ev2.Lat) ** 2.
-        let varLong = (ev1.Long - ev2.Long) ** 2.
-        varDays + varLat + varLong |> sqrt
+
 module Config =
     let [<Literal>] BingKey = "AptuYj2KB2MVNVuqw3b9pl33v3ba9KPTirN-2OKXLw3Y6ldA97CtBX2ibWZhN9GH"
 module Bing = 
@@ -56,10 +55,9 @@ module Bing =
         with e -> 
           printfn "[ERROR] Bing failed: %A" e
           None 
-let loc = Bing.locate "Los angeles"
+
 type BanksyScraper = HtmlProvider<"https://www.canvasartrocks.com/blogs/posts/70529347-121-amazing-banksy-graffiti-artworks-with-locations">
 let a = BanksyScraper.Load("https://www.canvasartrocks.com/blogs/posts/70529347-121-amazing-banksy-graffiti-artworks-with-locations")
-
 
 let yrPattern = "(20\d{2})"
 let latlongPattern = "(\d+\.\d+,-?\d+.\d+)"
@@ -69,62 +67,135 @@ let els = a.Html.Descendants("P")
             |> List.ofSeq
             |> List.filter (fun (a,b,c) -> (b.IsEmpty && c.IsEmpty) |> not)
 
-let banksys= [ for i in 1 .. 2 .. els.Length - 2 -> els.[i], els.[i + 1] ]
-               |> List.filter (fun ((imgP,img,mapA'),(mapP,img',mapA)) -> (not (img.IsEmpty) && (mapA'.IsEmpty)) && ((img'.IsEmpty) && not (mapA.IsEmpty)))
-               |> List.map ( fun ((imgP,img,mapA'),(mapP,img',mapA)) -> let year = Regex.Match(mapP.InnerText(), yrPattern)
-                                                                        let imgsrc = img.[0].AttributeValue("src")
-                                                                        let latLong = Regex.Match(mapA.[0].Attribute("href").Value(), latlongPattern)
-                                                                        let name = img.[0].AttributeValue("alt")
-                                                                        year,imgsrc,latLong,name
-                           )
-               |> List.filter (fun (yr,img,latlong,name) -> yr.Success && latlong.Success)
-               |> List.map    (fun (yr,img,latlong,name) -> let latlong' = latlong.Value.Split ',' |> Array.map float
-                                                            {Occurred=DateTime(yr.Value |> int,1,1);ImgSrc=img; Lat=latlong'.[0];Long=latlong'.[1];Name=name})
-               |> List.sortBy (fun e -> e.Occurred)
+let banksysByYear = 
+    [ for i in 1 .. 2 .. els.Length - 2 -> els.[i], els.[i + 1] ]
+        |> List.filter (fun ((imgP,img,mapA'),(mapP,img',mapA)) -> (not (img.IsEmpty) && (mapA'.IsEmpty)) && ((img'.IsEmpty) && not (mapA.IsEmpty)))
+        |> List.map ( fun ((imgP,img,mapA'),(mapP,img',mapA)) -> let year = Regex.Match(mapP.InnerText(), yrPattern)
+                                                                 let imgsrc = img.[0].AttributeValue("src")
+                                                                 let latLong = Regex.Match(mapA.[0].Attribute("href").Value(), latlongPattern)
+                                                                 let name = img.[0].AttributeValue("alt")
+                                                                 year,imgsrc,latLong,name
+                    )
+        |> List.filter (fun (yr,img,latlong,name) -> yr.Success && latlong.Success)
+        |> List.map    (fun (yr,img,latlong,name) -> let latlong' = latlong.Value.Split ',' |> Array.map float
+                                                     {Occurred=DateTime(yr.Value |> int,1,1);ImgSrc=img; Lat=latlong'.[0];Long=latlong'.[1];Name=name})
+        |> List.groupBy (fun e -> e.Occurred.Year) |> dict
+
 type MassiveAttackEvents = HtmlProvider<"http://www.bandsintown.com/MassiveAttack/past_events?page=10">
 
-let englicise (s:string) =
-    let repls = [('�','U');('�','O');('�','O')] |> dict
-    new String(s |> Seq.map (fun c -> match repls.TryGetValue(c) with
-                                      |true,v -> v
-                                      |fale,_ -> c) |> Array.ofSeq)
 let mattaks = 
     [for i in 1 ..13 ->
         let dates = MassiveAttackEvents.Load(sprintf "http://www.bandsintown.com/MassiveAttack/past_events?page=%d" i)
         dates.Tables.``Past Dates``.Rows |> Seq.map (fun r -> 
-                        let city = r.Location.Split([|','|]).[0].ToUpper() //|> englicise
-                        let country = r.Location.Split([|','|]).[1].ToUpper() //|> englicise
+                        let city = r.Location.Split([|','|]).[0].ToUpper()
+                        //let country = r.Location.Split([|','|]).[1].ToUpper()
                         let lat,long,name = match Bing.locate city with
                                             |Some(lat,long) -> lat,long,city
-                                            |None           -> match Bing.locate country with
-                                                               |Some(lt,lng) -> lt,lng,country
-                                                               |None         -> 0.,0.,country 
-                        {Occurred = r.Date; Name = city; Lat= lat; Long = long ; ImgSrc=""})
+                                            |None           -> 0.,0.,city 
+                        {Occurred = r.Date; Name = city; Lat= lat; Long = long ; ImgSrc="/3d.jpg"})
                         |> List.ofSeq
     ] |> List.concat |> List.sortBy (fun e -> e.Occurred)
-(*
-let missingGeo = mattaks |> List.filter (fun e -> e.Lat = 0.)
-let mataksByYear = mattaks |> List.countBy (fun e -> e.Occurred.Year)
-let baknsyByYear = banksys |> List.countBy (fun e -> e.Occurred.Year)
-*)
-let evt = Event<string>()
+
+//Suave bits
+
 let timer = new Timers.Timer(interval = 1000. , Enabled=true)
-let socketHandler (webSocket : WebSocket) =
-    fun cx -> socket {
-        while true do
-            let! evtData =
-                Control.Async.AwaitEvent(evt.Publish)
-                |> Suave.Sockets.SocketOp.ofAsync
-            do! webSocket.send Text (ASCII.bytes evtData) true
-    }
+// Cached version of: https://en.wikipedia.org/wiki/List_of_time_zones_by_country
+type TimeZones = HtmlProvider<"data/List_of_time_zones_by_country.html">
+let reg = System.Text.RegularExpressions.Regex("""UTC([\+\-][0-9][0-9]\:[0-9][0-9])?""")
+
+let explicitZones = 
+  [ "France", "UTC+01:00"; "United Kingdom", "UTC"; "Kingdom of Denmark", "UTC+01:00"
+    "Netherlands", "UTC+01:00"; "Portugal", "UTC"; "Spain", "UTC"; 
+    "Northern Cyprus", "UTC+02:00"; "Denmark", "UTC+01:00"; "Greenland", "UTC-03:00"
+    "North Korea", "UTC+08:30"; "South Korea", "UTC+09:00"; "New Caledonia", "UTC+11:00"
+    "Somaliland", "UTC+03:00"; "Republic of Serbia", "UTC+01:00"
+    "United Republic of Tanzania", "UTC+03:00"; "United States of America", "UTC-06:00" ]
+
+/// Returns a list of pairs with country name and its time zone
+let timeZones = 
+ [| let special = dict explicitZones 
+    for k, v in explicitZones do
+      yield k, v
+    for r in TimeZones.GetSample().Tables.Table1.Rows do
+      if not (special.ContainsKey r.Country) then 
+        let tz = r.``Time Zone``.Replace("−", "-")
+        let matches = reg.Matches(tz)
+        if matches.Count > 0 then
+          yield r.Country, matches.[matches.Count/2].Value |] 
+
+/// Returns a list of time zones, sorted from -7:00 to +13:00
+let zoneSequence = 
+  timeZones |> Seq.map snd |> Seq.distinct |> Seq.sortBy (fun s -> 
+    match s.Substring(3).Split(':') with
+    | [| h; m |] -> int h, int m
+    | _ -> 0, 0 )
+/// Use JSON type provider to generate types for the things we are returning
+type JsonTypes = JsonProvider<"""{
+    "socketMapEvent": 
+      { "latitude":50.07, "longitude":78.43, "text":"hello", "occurred" : "01/01/00",
+        "picture":"http://blah.jpg", "name":"sillyjoe" },
+    "timeZoneInfo": 
+      { "countries": [ {"country": "UK", "zone": "UTC" }, {"country": "UK", "zone": "UTC" } ],
+        "zones": [ "UTC", "UTC+00:00" ] }
+  }""">
+
+let _,mattakGigEvents =
+    timer.Elapsed |> Observable.scan (fun count _ -> count + 1) 0 
+                  |> Observable.map  (fun i -> printfn "%A" i
+                                               let event = mattaks.[i % mattaks.Length]
+                                               JsonTypes.SocketMapEvent(event.Lat  |> decimal, 
+                                                                        event.Long |> decimal,
+                                                                        event.Name,
+                                                                        event.Occurred,
+                                                                        event.ImgSrc,
+                                                                        "3d").JsonValue.ToString()
+                                    )
+                  |> Observable.start
+let _,banksyEvents =
+    timer.Elapsed |> Observable.scan (fun count _ -> count + 1) 0 
+                  |> Observable.map  (fun i -> printfn "%A" i
+                                               let year = (mattaks.[i % mattaks.Length]).Occurred.Year
+                                               let yearBanksys = match banksysByYear.TryGetValue year with
+                                                                 |true,bs->
+                                                                       printfn "%A" bs
+                                                                       bs |> List.map (fun event ->
+                                                                               JsonTypes.SocketMapEvent(event.Lat  |> decimal, 
+                                                                                                        event.Long |> decimal,
+                                                                                                        event.Name,
+                                                                                                        event.Occurred,
+                                                                                                        event.ImgSrc,
+                                                                                                        "banksy").JsonValue) |> Array.ofList
+                                                                 |false,_ -> [||]
+                                               JsonValue.Array(yearBanksys).ToString()
+                                    )
+                  |> Observable.start
+
+// Passes updates from IObservable<string> to a socket
+let socketOfObservable (updates:IObservable<string>) (webSocket:WebSocket) cx = socket {
+  while true do
+    let! update = updates |> Async.AwaitObservable |> Suave.Sockets.SocketOp.ofAsync
+    do! webSocket.send Text (System.Text.Encoding.UTF8.GetBytes update) true }
+
+/// Time-zone information (calcualted just once) packaged as a JSON
+let timeZonesJson = 
+  (JsonTypes.TimeZoneInfo
+    (Array.map (fun (c, z) -> JsonTypes.Country(c, z)) timeZones, 
+     Array.ofSeq zoneSequence)).ToString()
+
 let webPart =
     choose [
-        path "/events" >=> handShake socketHandler
-        pathRegex "(.*)\.(css|js|htm)" >=> Files.browseHome
+        path "/mattaks" >=> handShake (socketOfObservable mattakGigEvents)
+        path "/banksys" >=> handShake (socketOfObservable banksyEvents)
+        path "/zones" >=> Successful.OK timeZonesJson
+        pathRegex "(.*)\.(css|js|html|jpg)" >=> Files.browseHome
     ]
 let config = { defaultConfig with
-                   homeFolder = Some(__SOURCE_DIRECTORY__ + "/web/")
+                   homeFolder = Some(__SOURCE_DIRECTORY__ + @"\web")
                    bindings = 
                         [ HttpBinding.mk HTTP (System.Net.IPAddress.Parse "0.0.0.0") 8082us ]
              }
-startWebServerAsync config webPart
+
+let start, run = startWebServerAsync config webPart
+let ct = new System.Threading.CancellationTokenSource()
+Async.Start(run, ct.Token)
+System.Diagnostics.Process.Start("chrome.exe","http://localhost:8082/index.html")
